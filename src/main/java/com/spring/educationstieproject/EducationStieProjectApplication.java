5831be76fa3cdda2836da63bf3e5eaa29445c47e
mabbakk <siyun145928@gmail.com>
2025-01-15 14:00:26 +0900

    initial commit

---
+++ src/main/java/com/spring/educationstieproject/EducationStieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.educationstieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationStieProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationStieProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/educationstieproject/EducationStieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.educationstieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationStieProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
